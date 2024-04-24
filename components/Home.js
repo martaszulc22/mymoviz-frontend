a94d7a16f9ca07e39e449373b1aa223b0dfa4f56
Marta Szulc <marta.szulc2202@gmail.com>
2024-04-24 18:12:17 +0200

    commit

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('mymoviz-backend-ashen.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const modifiedMoviesData = data.movies.map(movie => ({
